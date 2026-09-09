ac5a1959265567b22ba98c4a57d749a3749fa3f4
Irakli Grigolia <igrigolia@wpi.edu>
2026-09-09 15:21:32 +0300

    Signed-out visitors on /play go to their locale hub (except the sign-in entry)

--- src/app/(app)/play/page.tsx
+++ src/app/(app)/play/page.tsx
@@ -2,10 +2,12 @@
 
 import { Suspense } from "react";
 import { PlayHome } from "@/features/play/PlayHome";
+import { GuestHubRedirect } from "@/features/play/GuestHubRedirect";
 
 export default function PlayPage() {
   return (
     <Suspense fallback={null}>
+      <GuestHubRedirect />
       <PlayHome />
     </Suspense>
   );
